9ccf804782a3506235458139e3a71b1551470688
SChaparro <81198964+SantiChaparro@users.noreply.github.com>
2025-09-03 21:42:15 -0300

    cambiosd en llamada a la ruta desde front

--- FrontEnd/src/components/Form/form.jsx
+++ FrontEnd/src/components/Form/form.jsx
@@ -30,7 +30,7 @@ const Form = () => {
     e.preventDefault();
     setLoading(true);
     try {
-      const resp = await axios.post("http://localhost:3001/quote/quoteRequest", formData);
+      axios.post("https://aldux.com.ar/server/quote/quoteRequest", formData);
       if (resp.data.message) {
         Swal.fire({
           title: '¡Éxito!',
